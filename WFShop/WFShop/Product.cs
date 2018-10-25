6837fa191665b1e334b2fa10696da841c29453f3
AnorZaken <AnorZaken@users.noreply.github.com>
2018-10-25 15:12:51 +0200

    Lagt till ProductCategory och likhetsmetoder m.m. på Product.

--- WFShop/WFShop/Product.cs
+++ WFShop/WFShop/Product.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WFShop
 {
-    class Product
+    class Product : IEquatable<Product>
     {
         public int SerialNumber { get; }
         public string Name { get; }
         public decimal Price { get; }
+        public ProductCategory Category { get; }
         public string Description { get; }
 
-        public Product(int serialNumber, string name, decimal price, string desc)
+        public Product(int serialNumber, string name, decimal price, string category , string desc)
         {
             SerialNumber = serialNumber;
             Name = name;
             Price = price;
+            Category = new ProductCategory(category);
             Description = desc;
         }
+
+        // Beteende: det som avgör likhet är serienummer, pris, och namn - inget annat!
+        public bool Equals(Product other)
+            => other != null && (SerialNumber == other.SerialNumber & Price == other.Price & Name == other.Name);
+
+        public override bool Equals(object obj)
+            => obj is Product other && this.Equals(other);
+
+        public static bool operator ==(Product a, Product b)
+            => a != null && a.Equals(b);
+        public static bool operator !=(Product a, Product b)
+            => a == null || !a.Equals(b);
+
+        public override int GetHashCode()
+            => Price.GetHashCode() + (Name.GetHashCode() + SerialNumber * 29) * 23; // 23 and 29 are prime.
+
+        public override string ToString()
+            => $"{Name ?? "<Unknown Product>"} ({SerialNumber})";
 
         // TODO ...
     }
---
+++ WFShop/WFShop/ProductCategory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WFShop
+{
+    // gör lite fula trick kanske :P
+    public readonly struct ProductCategory : IEquatable<ProductCategory>, IEquatable<string>
+    {
+        public string Name { get; }
+        public string Description => GetDescription(Name);
+
+        public ProductCategory(string name)
+        {
+            Name = name;
+        }
+        
+        public ProductCategory(string name, string desc)
+        {
+            Name = name;
+            descriptions[name] = desc;
+        }
+
+        private static readonly Dictionary<string, string> descriptions = new Dictionary<string, string>();
+
+        public static string GetDescription(string categoryName)
+            => descriptions.TryGetValue(categoryName, out string desc) ? desc : null;
+
+        public static void SetDescription(string categoryName, string description)
+            => descriptions[categoryName] = description;
+
+        // Beteende: null != null då det antagligen inte är användbart att betrakta dem som equivalenta.
+        public bool Equals(string categoryName)
+            => Name != null & Name == categoryName;
+
+        // Beteende: null != null då det antagligen inte är användbart att betrakta dem som equivalenta.
+        public bool Equals(ProductCategory other)
+            => Name != null & Name == other.Name;
+
+        public override bool Equals(object obj)
+            => obj is ProductCategory other && this.Equals(other);
+
+        public static bool operator ==(ProductCategory a, ProductCategory b)
+            => a.Equals(b);
+        public static bool operator !=(ProductCategory a, ProductCategory b)
+            => !a.Equals(b);
+
+        public override int GetHashCode()
+            => Name == null ? 0 : Name.GetHashCode();
+
+        public override string ToString()
+            => Name ?? "";
+    }
+}
