b90fa0720da52d3f53176a83ebabcdf93c21d7dc
AnorZaken <AnorZaken@users.noreply.github.com>
2018-11-01 11:10:53 +0100

    En abstract Discount basklass + lite relaterade interface.

---
+++ WFShop/WFShop/IDiscount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WFShop
+{
+    interface IDiscount
+    {
+        string Name { get; }
+        string Description { get; }
+
+        // Null if this is a fixed rebate and not a cupon-code.
+        string CuponCode { get; }
+
+        // Applies to [a] specific product[s] only?
+        bool IsProductSpecific { get; }
+
+        // Note: returns -1 if not product specific.
+        int ProductSerialNumber { get; }
+
+        bool DoesApply(List<Product> products);
+        decimal Calculate(List<Product> products);
+    }
+}
