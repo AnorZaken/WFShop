aaa6f8588d09d58355cda0f0f6f2461e6fe5d561
AnorZaken <AnorZaken@users.noreply.github.com>
2018-11-07 14:43:34 +0100

    Added Discount class: TotalPercentageCoupon + minor BuyXPayForY changes.

--- WFShop/WFShop/Discounts/BuyXPayForY.cs
+++ WFShop/WFShop/Discounts/BuyXPayForY.cs
@@ -5,14 +5,13 @@ namespace WFShop.Discounts
 {
     class BuyXPayForY : Discount
     {
+        const string TYPE = "BX4PY";
+
         private BuyXPayForY(string name, string desc, int productSerialNumber, int buyX, int payY)
-            : base(name, desc, "BuyXPayForY", productSerialNumber)
+            : base(name, desc, TYPE, productSerialNumber)
         {
             this.BuyX = buyX;
             this.PayY = payY;
-
-            if (payY <= 0 || buyX <= payY)
-                throw new ArgumentException("Requirement unmet: Buy quantity > Pay quantity > 0");
         }
 
         public readonly int BuyX;
@@ -36,35 +35,41 @@ namespace WFShop.Discounts
             && amount >= BuyX;
 
         #region --- Parsing (static) ---
-
-        public static readonly IDiscountParser<BuyXPayForY> Parser = new ParserClass();
+        /* Some third party must call the RegisterParser() function to make this
+         * Discount type/class available for parsing from Discount.TryParse(...)
+         */
+        public static readonly IDiscountParser<BuyXPayForY> Parser = new DP();
 
         public static void RegisterParser()
             => RegisterParser(Parser);
 
-        private class ParserClass : IDiscountParser<BuyXPayForY>
+        private class DP : IDiscountParser<BuyXPayForY>
         {
             private static readonly string[] keys =
             {
                 "Type", "Name", "Desc", "ProductSN", "BuyX", "PayY"
             };
 
-            const string TYPE = "BX4PY";
-
             public BuyXPayForY ParseOrNull(IDictionary<string, string> parsedValues)
-             => parsedValues.Count == keys.Length &&
-                parsedValues.TryGetValue(keys[0], out string type) &&
-                StringComparer.OrdinalIgnoreCase.Equals(type, TYPE) &&
-                parsedValues.TryGetValue(keys[1], out string name) &&
-                parsedValues.TryGetValue(keys[2], out string desc) &&
-                parsedValues.TryGetValue(keys[3], out string s_psn) &&
-                parsedValues.TryGetValue(keys[4], out string s_buy) &&
-                parsedValues.TryGetValue(keys[5], out string s_pay) &&
-                int.TryParse(s_psn, out int psn) &&
-                int.TryParse(s_buy, out int buy) &&
-                int.TryParse(s_pay, out int pay)
-                ? new BuyXPayForY(name, desc, psn, buy, pay)
-                : null;
+            {
+                if (parsedValues.Count == keys.Length &&
+                    parsedValues.TryGetValue(keys[0], out string type) &&
+                    StringComparer.OrdinalIgnoreCase.Equals(type, TYPE) &&
+                    parsedValues.TryGetValue(keys[1], out string name) &&
+                    parsedValues.TryGetValue(keys[2], out string desc) &&
+                    parsedValues.TryGetValue(keys[3], out string s_psn) &&
+                    parsedValues.TryGetValue(keys[4], out string s_buy) &&
+                    parsedValues.TryGetValue(keys[5], out string s_pay) &&
+                    int.TryParse(s_psn, out int psn) &&
+                    int.TryParse(s_buy, out int buy) &&
+                    int.TryParse(s_pay, out int pay))
+                {
+                    if (pay <= 0 || buy <= pay)
+                        throw new FormatException("Requirement unmet: Buy quantity > Pay quantity > 0");
+                    return new BuyXPayForY(name, desc, psn, buy, pay);
+                }
+                return null;
+            }
         }
         #endregion
     }
---
+++ WFShop/WFShop/Discounts/TotalPercentageCoupon.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFShop.Discounts
+{
+    class TotalPercentageCoupon : Discount
+    {
+        const string TYPE = @"T%C";
+
+        private TotalPercentageCoupon(string name, string desc, string couponCode, decimal percentage, decimal minimumCartValue)
+            : base(name, desc, TYPE, couponCode: couponCode)
+        {
+            this.Percentage = percentage;
+            this.MinCartValue = minimumCartValue;
+        }
+
+        public readonly decimal Percentage;
+        public readonly decimal MinCartValue;
+
+        private decimal Sum(IReadOnlyDictionary<Product, int> cart)
+            => cart.Sum(kvp => kvp.Key.Price * kvp.Value);
+
+        public override decimal Calculate(IReadOnlyDictionary<Product, int> cart)
+        {
+            var sum = Sum(cart);
+            return sum >= MinCartValue ? sum * Percentage : 0;
+        }
+
+        public override bool IsApplicable(IReadOnlyDictionary<Product, int> cart)
+            => Sum(cart) >= MinCartValue;
+
+        #region --- Parsing (static) ---
+        /* Some third party must call the RegisterParser() function to make this
+         * Discount type/class available for parsing from Discount.TryParse(...)
+         */
+        public static readonly IDiscountParser<TotalPercentageCoupon> Parser = new DP();
+
+        public static void RegisterParser()
+            => RegisterParser(Parser);
+
+        private class DP : IDiscountParser<TotalPercentageCoupon>
+        {
+            private static readonly string[] keys =
+            {
+                "Type", "Name", "Desc", "CouponCode", "Percentage", "MinCartValue"
+            };
+
+            public TotalPercentageCoupon ParseOrNull(IDictionary<string, string> parsedValues)
+            {
+                if (parsedValues.Count == keys.Length &&
+                    parsedValues.TryGetValue(keys[0], out string type) &&
+                    StringComparer.OrdinalIgnoreCase.Equals(type, TYPE) &&
+                    parsedValues.TryGetValue(keys[1], out string name) &&
+                    parsedValues.TryGetValue(keys[2], out string desc) &&
+                    parsedValues.TryGetValue(keys[3], out string couponCode) &&
+                    parsedValues.TryGetValue(keys[4], out string s_percent) &&
+                    parsedValues.TryGetValue(keys[5], out string s_minValue) &&
+                    decimal.TryParse(s_percent, out decimal percentage) &&
+                    decimal.TryParse(s_minValue, out decimal minValue))
+                {
+                    couponCode = couponCode?.Trim();
+                    if (!(couponCode?.Length >= 3))
+                        throw new FormatException("Coupon codes must be at least 3 characters long (ignoring leading / trailing whitespace)");
+                    if (percentage <= 0 || percentage >= 1)
+                        throw new FormatException("Percentage must be greater than 0.0 and less than 1.0.");
+                    if (minValue < 0)
+                        throw new FormatException("MinCartValue cannot be negative.");
+                    return new TotalPercentageCoupon(name, desc, couponCode, percentage, minValue);
+                }
+                return null;
+            }
+        }
+        #endregion
+    }
+}
